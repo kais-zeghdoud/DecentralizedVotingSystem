1bdf9573fbf51350a155204f5287806f5fe4a3b8
Kais <kais.zeghdoud@efrei.net>
2024-03-08 22:20:24 +0100

    Adding flask api

---
+++ api/views.py
@@ -0,0 +1,9 @@
+import sys, subprocess, logging
+from flask import Flask, render_template, request, redirect, flash, url_for
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('./../html/index.html')
