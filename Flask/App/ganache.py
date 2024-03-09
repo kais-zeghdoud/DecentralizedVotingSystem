0092a11f14f13d9e44c18982b1dbb97a8fc84e1a
Kais <kais.zeghdoud@efrei.net>
2024-03-09 23:44:21 +0100

    Adding ganache-cli keys

--- Flask/App/ganache.py
+++ Flask/App/ganache.py
@@ -1,17 +1,14 @@
+import json
 from web3 import Web3
 
-ganache_url = "http://127.0.0.1:7545"
+ganache_url = "http://127.0.0.1:8545"
 web3 = Web3(Web3.HTTPProvider(ganache_url))
-
+ganache_cli_command = "ganache-cli --accounts=100 ---account_keys_path /Flask/App/keys.json"
 
 def get_accounts():
     if web3.isConnected():
-        accounts = {}
-        for account in web3.eth.accounts:
-            address = account
-            pk = web3.eth.account.privateKeyToAccount(account).privateKey
-            accounts[address] = pk
-        return accounts
+        with open('Flask/App/keys.json', 'r') as file:
+            keys = json.load(file)
+        return keys["private_keys"]
     else:
         return False
-print(get_accounts())
