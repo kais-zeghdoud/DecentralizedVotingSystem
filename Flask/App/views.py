430b135e65d0806ae72ec50863b55dfc96b1709a
Kais <kais.zeghdoud@efrei.net>
2024-03-10 00:11:41 +0100

    Adding flash messaging

--- Flask/App/views.py
+++ Flask/App/views.py
@@ -10,8 +10,9 @@ data = {'name': "kais",
         'address': '0x95222290DD7278Aa3Ddd389Cc1E1d165CC4BAfe5',
         'pk': '0x999999cf1046e68e36E1aA2E0E07105eDDD1f08E',
         'votes': 0}
-app = Flask(__name__)
 
+app = Flask(__name__)
+app.secret_key = "secret_key"
 
 @app.route('/VotinCorpo')
 def index():
@@ -43,11 +44,11 @@ def login():
                 if "0x"+accounts[address] == pk:
                     return render_template("elector.html", user = data)
                 else:
-                    print("Invalid private key !")
+                    flash("Invalid private key!")
             else:
-                print("Invalid Address")
+                flash("Invalid Address...")
         else:
-            print("blockchain unreachable, make sure that ganache is launched")
+            flash("Blockchain unreachable, make sure that ganache is launched")
     return render_template('login.html')
 
 
