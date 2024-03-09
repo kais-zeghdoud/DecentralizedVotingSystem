b50cb3ed412cf4a67140710b4f79f1b21469902f
Kais <kais.zeghdoud@efrei.net>
2024-03-09 21:10:19 +0100

    Adding copy and hide functions

--- Flask/App/static/js/vote.js
+++ Flask/App/static/js/vote.js
@@ -4,6 +4,29 @@ function openPopup() {
 function closePopup() {
     document.getElementById("votePopup").style.display = "none";
 }
+let isTextVisible = true;
+let initialText = toggleVisibility();
+function toggleVisibility() {
+    const pkElement = document.getElementById('private-key');
+    const pkText = pkElement.innerText;
+    console.log(pkText);
+    const asterisks = '*'.repeat(pkText.length);
+    pkElement.innerText = isTextVisible ? asterisks : pkText;
+    isTextVisible = !isTextVisible;
+    if (pkText != asterisks)
+        return pkText;
+}
+
+
+function copyToClipboard(text) {
+    const el = document.createElement('textarea');
+    el.value = text;
+    document.body.appendChild(el);
+    el.select();
+    document.execCommand('copy');
+    document.body.removeChild(el);
+    alert('Private key copied to clipboard');
+}
 
 // Récupération du chemin de la page actuelle
 var currentPage = window.location.pathname;
