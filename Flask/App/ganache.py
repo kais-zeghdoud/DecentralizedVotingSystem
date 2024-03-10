8b020960762b54199639cbbe1b6f5b760c058e48
Kais <kais.zeghdoud@efrei.net>
2024-03-10 19:11:01 +0100

    Adding ganache-cli flags

--- Flask/App/ganache.py
+++ Flask/App/ganache.py
@@ -5,7 +5,7 @@ ganache_url = "http://127.0.0.1:8545"
 web3 = Web3(Web3.HTTPProvider(ganache_url))
 
 def launch_blockchain():
-    subprocess.run("ganache-cli --accounts=100 -i 1 --deterministic --db ./blockchain --account_keys_path ./blockchain/keys.json", shell=True)
+    subprocess.run("ganache-cli --accounts=100 --networkId 1 --gasPrice 0 --deterministic --db ./blockchain --account_keys_path ./blockchain/keys.json", shell=True)
 
 def get_accounts():
     if web3.isConnected():
